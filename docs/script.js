52b00b865b1671ed689657cd8ede8f0241e49357
Some User <user@localhost>
2019-07-30 14:30:13 +0200

    cleanup

--- docs/script.js
+++ docs/script.js
@@ -2,12 +2,24 @@ var home = location.href;
 if (location.protocol=="file:")
     var home = 'https://checkered-rabbit.github.io/proxy-git/';
 {
-let [_,orgName,repoName] = home.match(/\/\/(.*?)\..*\/(.*)\//);
-let glitchImport = 
-    `https://glitch.com/edit/#!/import/github/` +
-    `${orgName}/${repoName}` + 
-    `?org=${orgName}&repo=${repoName}`;
-console.log(glitchImport);
-document.writeln(
-    `<a href="${glitchImport}">Remix on Glitch</a>`);
+    let [_,orgName,repoName] = home.match(/\/\/(.*?)\..*\/(.*)\//);
+    let glitchImport = 
+        `https://glitch.com/edit/#!/import/github/` +
+        `${orgName}/${repoName}` + 
+        `?org=${orgName}&repo=${repoName}`;
+    console.log(glitchImport);
+
+    document.writeln(
+        `<h1>${orgName}/${repoName}</h1>`
+    )
+
+    document.writeln(
+        `<a href="https://github.com/${orgName}/` + 
+        `${repoName}">Github Repo</a><p>`
+    )
+
+    document.writeln(
+        `<a href="${glitchImport}">Remix on Glitch</a>`
+    )
+
 }
