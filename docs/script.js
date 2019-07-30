8d4cfd58417b8c5968cca30842a20dccd455d715
Some User <user@localhost>
2019-07-30 13:39:01 +0200

    getting project

--- docs/script.js
+++ docs/script.js
@@ -4,4 +4,8 @@ var home = location.href;
 if (location.protocol=="file:")
     var home = 'https://checkered-rabbit.github.io/proxy-git/';
 document.writeln(home);
+{
+let project = home.match(/.*\/(.*)\//)[1];
+debugger;
+}
 document.writeln('</pre>');
